aae62d2738ddafb6e456a7a0fc768f5ee3f77bc2
sanket <sanketpatel2912@gmail.com>
2023-11-30 13:20:15 +0530

    Added Application class

---
+++ app/src/main/java/com/sanket/newsapp/NewsApplication.kt
@@ -0,0 +1,11 @@
+package com.sanket.newsapp
+
+import android.app.Application
+
+class NewsApplication : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
--- app/src/main/java/com/sanket/newsapp/ui/topheadline/TopHeadlineActivity.kt
+++ app/src/main/java/com/sanket/newsapp/ui/topheadline/TopHeadlineActivity.kt
@@ -1,9 +1,10 @@
-package com.sanket.newsapp
+package com.sanket.newsapp.ui.topheadline
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sanket.newsapp.R
 
-class MainActivity : AppCompatActivity() {
+class TopHeadlineActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
